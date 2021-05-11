e6e1472d50f568b07235e19666e5c1affb967a30
thalesousa <thalestjsb@gmail.com>
2021-05-11 12:17:48 -0300

    Adicionando componente de paginação

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import Link from 'next/link';
 import { CardItem } from '../components/CardItem';
 import { api } from '../services/api';
 import styles from './home.module.scss';
@@ -15,18 +16,29 @@ type Hero = {
 export default function Home() {
   const [heroes, setHeroes] = useState<Hero[]>([]);
   const [offset, setOffset] = useState(0);
+  const [total, setTotal] = useState(0);
+  const [limit, setLimit] = useState(12);
+  const [currentPage, setCurrentPage] = useState(1);
+
   useEffect(() => {
-    async function loadProducts() {
+    async function loadCharacters() {
       const response = await api.get(`/characters`, {
         params: {
           offset,
+          limit,
         }
       })
       setHeroes(response.data.data.results)
+      setTotal(response.data.data.total)
     }
 
-    loadProducts();
+    loadCharacters();
   }, [offset]);
+
+  function handleNextPage(){
+    setCurrentPage(currentPage + 1)
+    setOffset(offset + limit)
+  }
 
 
   return(
@@ -35,14 +47,20 @@ export default function Home() {
         <ul>
           {heroes.map(hero => {
             return (
-              <CardItem hero={hero} />
+              <Link href={`/hero/${hero.id}`} key={hero.id}>
+                <a>
+                  <CardItem hero={hero} />
+                </a>
+              </Link>
             )
           })}
         </ul>
+
+        <div className={styles.pagination}>
+          <button>Previous</button>
+          <button onClick={handleNextPage}>Next</button>
+        </div>
       </section>
     </div>
   )
 }
-
-// export  const getStaticProps: GetStaticProps = async () => {
-  // const {data} = await api.get('/characters')
